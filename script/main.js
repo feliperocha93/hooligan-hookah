2b7585f1bd73da4d637cd2ec866c252244992c1e
Felipe <fee.rocha@hotmail.com>
2019-09-29 17:19:40 -0300

    menu-mobile

--- script/main.js
+++ script/main.js
@@ -1,15 +1,17 @@
 const menuButton = document.querySelector('.top-header > i');
 const mainMenu = document.querySelector('.main-menu');
-const main = document.querySelector('main');
+const menuWidth = mainMenu.getBoundingClientRect().width;
 
-function mainClick() {
-    mainMenu.classList.remove('active');
-    main.removeEventListener('touchstart', mainClick);
+function outsideClick(e) {
+    if (e.touches[0].clientX > menuWidth) {
+        mainMenu.classList.remove('active');
+        document.removeEventListener('touchstart', outsideClick);
+    } 
 }
 
 function menuButtonClick() {
     mainMenu.classList.add('active');
-    main.addEventListener('touchstart', mainClick);
+    document.addEventListener('touchstart', outsideClick);
 }
 
 menuButton.addEventListener('touchstart', menuButtonClick);
