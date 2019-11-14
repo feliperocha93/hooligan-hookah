b7ee927baa001756d2f98ed45d00e1f0283a0631
feliperocha93 <fee.rocha@hotmail.com>
2019-11-14 19:37:46 -0300

    Adiconado Gulp, SaSS, ESLint

--- script/distribuidores.js
+++ script/distribuidores.js
@@ -5,7 +5,7 @@ const locations = [];
 fetch('/script/distribuidores.json')
   .then((response) => response.json())
   .then((json) => {
-    json.locations.forEach((element) => locations.push(element))
+    json.locations.forEach((element) => locations.push(element));
   });
 
 function round(x) {
@@ -19,27 +19,29 @@ function deg2rad(deg) {
 
 // Cálculo de distância (fórmula retirada do google)
 function CalcRadiusDistance(lat1, lon1, lat2, lon2) {
-  const RADIUSMILES = 3961;
+  // const RADIUSMILES = 3961;
   const RADIUSKILOMETERS = 6373;
-  const latR1 = this.deg2rad(lat1);
-  const lonR1 = this.deg2rad(lon1);
-  const latR2 = this.deg2rad(lat2);
-  const lonR2 = this.deg2rad(lon2);
+  const latR1 = deg2rad(lat1);
+  const lonR1 = deg2rad(lon1);
+  const latR2 = deg2rad(lat2);
+  const lonR2 = deg2rad(lon2);
   const latDifference = latR2 - latR1;
   const lonDifference = lonR2 - lonR1;
-  const a = Math.pow(Math.sin(latDifference / 2), 2) + Math.cos(latR1) * Math.cos(latR2) * Math.pow(Math.sin(lonDifference / 2), 2);
-  const c  = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const dm = c * RADIUSMILES;
+  const a = (Math.sin(latDifference / 2) ** 2) + Math.cos(latR1)
+  * Math.cos(latR2)
+  * (Math.sin(lonDifference / 2) ** 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  // const dm = c * RADIUSMILES;
   const dk = c * RADIUSKILOMETERS;
-  this.mi = this.round(dm);
-  this.km = this.round(dk);
+  // const mi = round(dm);
+  const km = round(dk);
   return km;
 }
 
 // Obtém as coordenadas
 function showPosition(position) {
-  const latitude = position.coords.latitude; 
-  const longitude = position.coords.longitude;
+  // const latitude = position.coords.latitude;
+  const { coords: { latitude, longitude } } = position;
   let shorterDistance = 100000;
   let closest;
   // Chama a função que calcula distancia e compara a do navegador com as do JSON
